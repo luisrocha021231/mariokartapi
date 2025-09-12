3530f0aeb440a48eb0931cf2a1a629d78ce666c8
Luis Angel Rocha ronquillo <luisangelrocharonquillo@MacBook-Air-de-Luis.local>
2025-09-11 22:15:46 -0600

    changes

--- src/main/java/com/mariokart/controllers/CharacterController.java
+++ src/main/java/com/mariokart/controllers/CharacterController.java
@@ -3,7 +3,9 @@ package com.mariokart.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +29,9 @@ public class CharacterController {
     @PostMapping
     public void addCharacter(@RequestBody Character character) {
         characterservice.addCharacter(character);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCharacter(@PathVariable Long id) {
     }
 }
